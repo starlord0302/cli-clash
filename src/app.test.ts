307bd536f376dfa76d2ff579cc61283d5831e6c9
starlord0302 <karoly0302@gmail.com>
2022-08-07 21:45:59 +0200

    feat(): setup project, add necessary packages

---
+++ src/app.test.ts
@@ -0,0 +1,7 @@
+import { greeter } from "./app";
+
+describe('App', () => {
+  it("should return a string with the passed argument", () => {
+    expect(greeter('Tom')).toBe('Hello, Tom!');
+  });
+});
