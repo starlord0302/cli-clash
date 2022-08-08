2f3a3208d958e70b00a15b5040246031601d8cb8
starlord0302 <karoly0302@gmail.com>
2022-08-09 00:45:12 +0200

    feat(src/interfaces): add attack() method to AbstractHero

--- src/interfaces/AbstractHero.ts
+++ src/interfaces/AbstractHero.ts
@@ -1,3 +1,4 @@
+import { AbstractWeapon } from "./AbstractWeapon";
 import { Hero } from "./Hero";
 import { Weapon } from "./Weapon";
 
@@ -7,18 +8,30 @@ export abstract class AbstractHero implements Hero{
   abstract ability: string;
   abstract armour: number;
   abstract evasion: number;
-  weapon: Weapon | undefined;
+  weapon: AbstractWeapon | undefined = undefined;
   name: string;
 
   constructor(name: string) {
     this.name = name;
   }
 
-  equipWeapon(weapon: Weapon) {
+  equipWeapon(weapon: AbstractWeapon) {
     this.weapon = weapon;
     if (this.weapon.whoCouldUse != this.type && this.weapon.whoCouldUse != 'everybody') {
       this.weapon.minDamage = 0;
       this.weapon.maxDamage = 0;
+    }
+  }
+
+  attack(opponent: Hero, intialDamageAdder: number, chanceToHit: number, evadeChance: number): number {
+    if (chanceToHit <= this.weapon!.hitChance * 100 && this.weapon!.maxDamage > 0) {
+      if (evadeChance >= opponent.evasion) {
+        return this.weapon!.getDamage(intialDamageAdder) - Math.floor(opponent.armour / 3);
+      } else {
+        return 0;
+      }
+    } else {
+      return 0;
     }
   }
 }
