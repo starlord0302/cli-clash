4061790d33a91860412b62e9d681fdc157bfb827
starlord0302 <karoly0302@gmail.com>
2022-08-09 15:43:44 +0200

    feat(src/classes): set Chance constructor to optional

--- src/classes/Chance.ts
+++ src/classes/Chance.ts
@@ -1,16 +1,18 @@
 import { generateRandomTo100 } from "../utility/random-generator";
 
 export class Chance {
-  abilityChance: number;
-  evasionChance: number;
-  hitChance: number;
-  damageToAdd: number;
+  abilityChance?: number;
+  evasionChance?: number;
+  hitChance?: number;
+  damageToAdd?: number;
 
-  constructor(abilityChance: number, evasionChance: number, hitChance: number, damageToAdd: number) {
-    this.abilityChance = abilityChance;
-    this.evasionChance = evasionChance;
-    this.hitChance = hitChance;
-    this.damageToAdd = damageToAdd;
+  constructor(abilityChance?: number, evasionChance?: number, hitChance?: number, damageToAdd?: number) {
+    if (abilityChance && evasionChance && hitChance && damageToAdd) {
+      this.abilityChance = abilityChance;
+      this.evasionChance = evasionChance;
+      this.hitChance = hitChance;
+      this.damageToAdd = damageToAdd;
+    }
   }
 
   randomizeChances(): void {
