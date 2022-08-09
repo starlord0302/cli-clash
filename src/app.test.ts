05d4d6c08c7dd59e879c986b26892dcbd16ffc0e
starlord0302 <karoly0302@gmail.com>
2022-08-09 23:10:54 +0200

    feat(src): delete app.test

--- src/app.test.ts
+++
@@ -1,7 +0,0 @@
-import { greeter } from './app';
-
-describe('App', () => {
-  it('should return a string with the passed argument', () => {
-    expect(greeter('Tom')).toBe('Hello, Tom!');
-  });
-});
