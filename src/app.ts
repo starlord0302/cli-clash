effb21f3611e6363bc122003a9aed07d543ec81a
starlord0302 <karoly0302@gmail.com>
2022-08-09 15:44:35 +0200

    feat(src/app): add example gameplay

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,12 @@
-export function greeter(name: string): string {
-  return `Hello, ${name}!`;
-}
+import { Arena } from "./classes/Arena";
+import { Warrior } from "./classes/heroes/Warrior";
+import { Sword } from "./classes/weapons/Sword";
+import { WarHammer } from "./classes/weapons/WarHammer";
+
+const bela = new Warrior('Bela');
+bela.equipWeapon(new WarHammer());
+
+const pista = new Warrior('Pista');
+pista.equipWeapon(new Sword());
+
+Arena.tournament(bela, pista);
